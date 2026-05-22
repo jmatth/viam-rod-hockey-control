caff9682e74fa8adffae16b814b482f13dd0e8b2
Joshua Matthews <josh@jmatth.com>
2026-05-22 12:04:55 -0400

    Using uv in build scripts

--- build.sh
+++ build.sh
@@ -5,14 +5,10 @@ cd `dirname $0`
 VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 
-if ! $PYTHON -m pip install pyinstaller -Uqq; then
-    exit 1
-fi
-
-$PYTHON -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
+uv run -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
 
 TAR_FILES="meta.json ./dist/main"
-FIRST_RUN=$($PYTHON -c "import json; print(json.load(open('meta.json')).get('first_run', ''))" 2>/dev/null)
+FIRST_RUN=$(uv run python -c "import json; print(json.load(open('meta.json')).get('first_run', ''))" 2>/dev/null)
 if [ -n "$FIRST_RUN" ] && [ -f "$FIRST_RUN" ]; then
     TAR_FILES="$TAR_FILES $FIRST_RUN"
 fi
